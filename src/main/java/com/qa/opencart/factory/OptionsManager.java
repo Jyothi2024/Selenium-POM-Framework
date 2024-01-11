6a96d8d54eaf6db5942c0ae6c914dc3b091fef48
Jyothi2024 <jyothi.qa2023@gmail.com>
2024-01-11 11:59:59 -0500

    updated remote excecution

--- src/main/java/com/qa/opencart/factory/OptionsManager.java
+++ src/main/java/com/qa/opencart/factory/OptionsManager.java
@@ -1,5 +1,7 @@
 package com.qa.opencart.factory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,6 +25,21 @@ public class OptionsManager {
 		co = new ChromeOptions();
 		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) co.addArguments("--headless");
 		if(Boolean.parseBoolean(prop.getProperty("incognito").trim())) co.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setCapability("browserName", "chrome");
+			co.setBrowserVersion(prop.getProperty("browserversion").trim());
+			
+//			Map<String, Object> selenoidOptions = new HashMap<>();
+//			selenoidOptions.put("screenResolution", "1280x1024x24");
+//			selenoidOptions.put("enableVNC", true);
+//			selenoidOptions.put("name", prop.getProperty("testname"));
+//			co.setCapability("selenoid:options", selenoidOptions);
+
+		}
+		
+		
+		
 		return co;
 	}
 	
@@ -30,6 +47,18 @@ public class OptionsManager {
 		fo = new FirefoxOptions();
 		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) fo.addArguments("--headless");
 		if(Boolean.parseBoolean(prop.getProperty("incognito").trim())) fo.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			fo.setCapability("browserName", "firefox");
+			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+			
+//			Map<String, Object> selenoidOptions = new HashMap<>();
+//			selenoidOptions.put("screenResolution", "1280x1024x24");
+//			selenoidOptions.put("enableVNC", true);
+//			selenoidOptions.put("name", prop.getProperty("testname"));
+//			fo.setCapability("selenoid:options", selenoidOptions);
+
+		}
 		return fo;
 	}
 	
